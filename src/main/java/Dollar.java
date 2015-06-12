c68b826b09ed72715600235cc8cae007540e4748
derek clark <derekclark14@googlemail.com>
2015-06-12 19:48:16 +0100

    after 1st chapter

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,12 @@
+public class Dollar {
+    public int amount=10;
+
+    public Dollar(int amount) {
+        this.amount=amount;
+    }
+
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
