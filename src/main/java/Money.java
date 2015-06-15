204743797448e8c5d8f8ba21cca954a16a9d2f26
derek clark <derekclark14@googlemail.com>
2015-06-15 15:15:33 +0100

    completed times we're livin' in chapter

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,11 @@
-public class Money {
+public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount=amount;
+        this.currency=currency;
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
@@ -7,10 +13,15 @@ public class Money {
     }
 
     public static Dollar dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount,"USD");
     }
 
     public static Franc franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
+
+    String currency() {
+        return currency;
+    }
+
 }
