19e24ee86d7a60d495de3fec4e7579a0fa2aeb54
derek clark <derekclark14@googlemail.com>
2015-06-15 16:05:51 +0100

    completed Addition finally chapter

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -30,5 +30,9 @@ public class Money {
 
     public String toString(){
         return amount + " " + currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount,currency);
     }
 }
--- test/main/java/MoneyTest.java
+++ test/main/java/MoneyTest.java
@@ -9,8 +9,8 @@ public class MoneyTest {
     @Test
     public void testMutiplication(){
         Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10),five.times(2));
-        assertEquals(Money.dollar(15),five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
@@ -24,6 +24,15 @@ public class MoneyTest {
     @Test
     public void testCurrency(){
         assertEquals("USD",Money.dollar(1).currency());
+    }
+
+    @Test
+    public void testSimpleAddition(){
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 
 }
