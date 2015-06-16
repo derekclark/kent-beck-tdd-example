9907193d816e3bacc53dfaf4cbb6f4b2fa42ec2b
derek clark <derekclark14@googlemail.com>
2015-06-16 18:55:44 +0100

    completed Mixed currencies chapter

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -11,7 +11,7 @@ public class Money implements Expression {
         this.currency=currency;
     }
 
-    Money times(int multiplier){
+    Expression times(int multiplier){
         return new Money(amount * multiplier, currency);
     }
 
@@ -32,7 +32,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
